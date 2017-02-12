63623a16d3923a0978ac75a75aa8af6ea8fb4d74
Mrinal Bhambhu <mrinalbhambhu@Mrinals-MacBook-Air.local>
2017-02-12 13:55:55 +0000

    footer file updated

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,7 +5,6 @@
 <!-- javascripts -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
-=======
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
 <script src="<?php echo base_url('assets/js/tether.min.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
